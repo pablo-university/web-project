927b8f894d857f22a771df4b45657df249b594e2
pabla <pablodelfante@vexels..com>
2022-10-04 21:46:41 -0300

    chore: create base menu

--- components/drawer/Trigger.jsx
+++ components/drawer/Trigger.jsx
@@ -1,7 +1,7 @@
 export default function Trigger({ children }) {
-    return (
-        <label htmlFor="my-drawer-4" className="btn btn-primary drawer-button">
-            {children}
-        </label>
-    );
+  return (
+    <label htmlFor="my-drawer-4" className="btn btn-primary drawer-button">
+      {children}
+    </label>
+  );
 }
--- components/layouts/LayoutWithDrawer.jsx
+++ components/layouts/LayoutWithDrawer.jsx
@@ -1,11 +1,13 @@
 import Drawer from "components/drawer/index";
+import Menu from "components/menu";
 
-export default function LayoutWithDrawer({children}) {
+export default function LayoutWithDrawer({ children }) {
   return (
     <Drawer aside={<p>content drawer</p>}>
-      <main>
-        {children}
-      </main>
+      <header>
+        <Menu></Menu>
+      </header>
+      <main>{children}</main>
     </Drawer>
-  )
+  );
 }
