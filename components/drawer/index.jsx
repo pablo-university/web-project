215a0ba776439530f51465092abe20bfa36f8144
pabla <pablodelfante@vexels..com>
2022-10-04 21:37:08 -0300

    chore: edit layout

--- components/drawer/index.jsx
+++ components/drawer/index.jsx
@@ -2,7 +2,6 @@ export default function index({ children, aside }) {
     return (
         <div className="drawer drawer-end">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-
             <div className="drawer-content">{children}</div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
--- components/layouts/LayoutWithDrawer.jsx
+++ components/layouts/LayoutWithDrawer.jsx
@@ -2,6 +2,10 @@ import Drawer from "components/drawer/index";
 
 export default function LayoutWithDrawer({children}) {
   return (
-    <Drawer aside={<p>content drawer</p>}>{children}</Drawer>
+    <Drawer aside={<p>content drawer</p>}>
+      <main>
+        {children}
+      </main>
+    </Drawer>
   )
 }
