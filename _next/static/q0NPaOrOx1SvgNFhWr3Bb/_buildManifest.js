107420a52806901cba2eeb8388987371a0169df8
pablodelfante <pablodelfante@users.noreply.github.com>
2022-10-08 14:34:57 +0000

    deploy: a1aed51cf19bb56fc2ad20a8a47042439b8e6388

--- _next/static/q0NPaOrOx1SvgNFhWr3Bb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4a7895010acacbfd.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],"/donar":["static/chunks/pages/donar-c441a9f2b850dac6.js"],sortedPages:["/","/_app","/_error","/donar"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
