356896f3935dd53c4fbe545757e0fae8215708eb
Pablo Delfante <66787488+pablodelfante@users.noreply.github.com>
2022-10-13 23:38:24 +0000

    test: try v2 fix problem on config conflicts to resolve

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
-import { BASE_PATH } from "utils";
 /** @type {import('next').NextConfig} */
-const BASE_PATH =   process.env.NODE_ENV === 'development' ? {} : {
-  basePath: BASE_PATH,
-}
+const BASE_PATH =
+    process.env.NODE_ENV === "development"
+        ? {}
+        : {
+              basePath: "/web-project",
+          };
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  ...BASE_PATH
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    ...BASE_PATH,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
