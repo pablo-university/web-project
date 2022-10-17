c6d4f587b45634e93f53442007f610a3b89fd47c
Pablo Delfante <66787488+pablodelfante@users.noreply.github.com>
2022-10-17 17:18:55 +0000

    feat: cover maria

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
 import Layout from "components/layouts/Layout";
 import Container from "components/layouts/Container";
 import Hero from "components/Hero";
+import coverMaria from "img/cover-maria.jpg";
 
 export default function Home() {
+    console.log(coverMaria);
     return (
         <Layout main={
             <main>
-                <Hero backgroundImageUrl={"https://placeimg.com/1000/800/arch"}>
+                <Hero backgroundImageUrl={coverMaria.src || "https://placeimg.com/1000/800/arch"}>
                     content
                 </Hero>
                 <Container>
