d740f56e2c71ebb2f6a00e99a296e34ee78d8c49
pabla <pablodelfantexp@gmail.com>
2022-10-20 22:41:37 -0300

    chore(container):

--- components/layouts/Container.jsx
+++ components/layouts/Container.jsx
@@ -1,3 +1,3 @@
 export default function Container({ className = '', children }) {
-  return <div className={`container m-auto ${className}`}>{children}</div>
+  return <div className={`container p-4 m-auto ${className}`}>{children}</div>
 }
