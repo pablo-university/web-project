58ac195fc54bb28e16f7972f305eb3a1ca121d0b
pabla <pablodelfantexp@gmail.com>
2022-10-22 16:27:08 -0300

    feat: create ContainerGrid and apply base styles

--- components/layouts/Divider.jsx
+++
@@ -1,8 +0,0 @@
-export default function Divider({ left, right, className }) {
-  return (
-    <div className={`grid ${className}`}>
-      <div>{left}</div>
-      <div>{right}</div>
-    </div>
-  )
-}
--- pages/donationPoints.jsx
+++ pages/donationPoints.jsx
@@ -1,10 +1,19 @@
-import Layout from "components/layouts/Layout";
-import Container from "components/layouts/Container";
+import Layout from 'components/layouts/Layout'
+import Container from 'components/layouts/Container'
+import Heading from 'components/layouts/Heading'
 
 export default function donationPoints() {
-    return (
-        <Layout>
-            <Container>puntos de donacion</Container>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Container>
+        <Heading
+          title="description"
+          description="description"
+          imageUrl="https://cdn.pixabay.com/photo/2022/10/16/13/53/early-morning-7525151_960_720.jpg"
+        >
+          mas contenido
+        </Heading>
+      </Container>
+    </Layout>
+  )
 }
