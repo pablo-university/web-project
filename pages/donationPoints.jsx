69767c47cbb17b5decd3e7e9763082e65daad0b0
pabla <pablodelfantexp@gmail.com>
2022-10-22 18:08:00 -0300

    style: footer changes

--- pages/donationPoints.jsx
+++ pages/donationPoints.jsx
@@ -18,9 +18,10 @@ export default function DonationPoints() {
     <Layout>
       <Container>
         <Heading title={title} description={description} imageUrl={src}>
-          mas contenido
+          --mas contenido
         </Heading>
       </Container>
+      <Container>--donation points</Container>
     </Layout>
   )
 }
