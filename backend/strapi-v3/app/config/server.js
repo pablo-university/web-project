715266ef9e3d9995f8a5ab92b46526b1c19aa091
pabla <pablodelfantexp@gmail.com>
2022-10-31 18:38:21 -0300

    remove folder backend

--- backend/strapi-v3/app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'aae511ad81ac826d8b37476cb6cbd03d'),
-    },
-  },
-});
--- backend/strapi-v3/app/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '7725461a-8a47-473a-bb98-78c82c27d7ab'
-};
