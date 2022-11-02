3e16ed5b257cc6033aad08e3813d30c26818b345
pabla <pablodelfante@vexels..com>
2022-11-01 22:09:01 -0300

    steps and donate working

--- pages/donate.jsx
+++ pages/donate.jsx
@@ -3,6 +3,8 @@ import Container from 'components/layouts/Container'
 import Heading from 'components/layouts/Heading'
 import reservationImg from 'img/reservation/reservation.svg'
 import ContainerGrid from 'components/layouts/ContainerGrid'
+import PrimaryButton from 'components/buttons/PrimaryButton'
+import TextInput from 'components/form/TextInput'
 
 export default function Donate() {
   return (
@@ -19,9 +21,21 @@ export default function Donate() {
         </Heading>
       </Container>
       <Container>
-        <ContainerGrid className="md:grid-cols-2">
-          <div>datos de la reserva</div>
+        <ContainerGrid className="md:grid-cols-2 gap-6">
           <div>
+            <h3>Datos de la reserva</h3>
+            <ContainerGrid className="lg:grid-cols-2 lg:gap-6">
+              <div>
+                <TextInput label="Localidad" />
+                <TextInput label="Motivo" />
+              </div>
+              <div>
+                <TextInput label="Fecha" />
+              </div>
+            </ContainerGrid>
+            <PrimaryButton>Siguiente</PrimaryButton>
+          </div>
+          <div className="grid gap-6">
             <h3>Tener en cuenta</h3>
             <ul>
               <li>Seleccione la localidad de donación</li>
@@ -36,7 +50,7 @@ export default function Donate() {
                 chequear la bandeja de SPAM)
               </li>
             </ul>
-            <ul className="steps">
+            <ul className="steps ">
               <li className="step step-primary">Datos de la reserva</li>
               <li className="step">Datos personales</li>
             </ul>
