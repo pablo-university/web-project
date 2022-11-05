c1c6140d81b0f1e51e1b7d94ea7138eec32b0e4d
pabla <pablodelfante@vexels..com>
2022-11-05 11:22:45 -0300

    edit condition card

--- components/cards/ConditionCard.jsx
+++ components/cards/ConditionCard.jsx
@@ -1,8 +1,8 @@
 export default function ConditionCard({ title, src, alt, className = '' }) {
   return (
-    <div className={`grid gap-2 p-6 md:p-8 border rounded-lg ${className}`}>
+    <div className={`grid gap-5 p-6 md:p-8 border rounded-lg ${className}`}>
       <img src={src} alt={alt} />
-      <h3 className="font-normal text-center">{title}</h3>
+      <h3 className="font-light text-center">{title}</h3>
     </div>
   )
 }
