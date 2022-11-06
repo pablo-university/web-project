1bd97d14f221715872d9f792eeb633a15644a9d9
pabla <pablodelfantexp@gmail.com>
2022-11-06 16:07:32 -0300

    feat: include NProgress dependency

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
+import Router from 'next/router'
 import '../styles/globals.css'
 import { BASE_PATH } from 'utils/index'
-import AppContextProvider from "context/AppContextProvider";
-
+import AppContextProvider from 'context/AppContextProvider'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 function MyApp({ Component, pageProps }) {
+  /* ... */
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start()
+    const handleRouteDone = () => NProgress.done()
+
+    Router.events.on('routeChangeStart', handleRouteStart)
+    Router.events.on('routeChangeComplete', handleRouteDone)
+    Router.events.on('routeChangeError', handleRouteDone)
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off('routeChangeStart', handleRouteStart)
+      Router.events.off('routeChangeComplete', handleRouteDone)
+      Router.events.off('routeChangeError', handleRouteDone)
+    }
+  }, [])
+  /* ... */
   return (
     <>
       <Head>
