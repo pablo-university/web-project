58d49ff58486e9d5f020f14dd170336a780afad3
pabla <pablodelfante@vexels..com>
2022-11-06 15:10:12 -0300

    fix error on articles reload

--- pages/articles.jsx
+++ pages/articles.jsx
@@ -25,7 +25,6 @@ export default function Articles() {
     )
     setArticlesFiltered(articlesFiltered)
   }
-
   return (
     <Layout>
       <ContainerSeparator>
@@ -59,7 +58,7 @@ export default function Articles() {
                   </Link>
                 )
               )}
-            {!articlesFiltered.length && (
+            {articlesFiltered && !articlesFiltered.length && (
               <Container>
                 <Alert>
                   No se encontraron artículos coincidentes con tu búsqueda
