505b31862b8706def0743522dfb0a0d7327ae148
pabla <pablodelfante@vexels..com>
2022-11-07 19:03:52 -0300

    get user attention

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero({ children, backgroundImageUrl }) {
   return (
     <div
-      className="hero min-h-screen"
+      className="hero min-h-[80vh]"
       style={{
         backgroundImage: `url(${backgroundImageUrl})`,
       }}
