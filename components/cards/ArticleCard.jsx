bbbcd5cbfc6596f2c9eee440af3c117d79ec9168
pabla <pablodelfante@vexels..com>
2022-11-07 19:29:02 -0300

    set static height on cards, warning with this tecnic

--- components/cards/ArticleCard.jsx
+++ components/cards/ArticleCard.jsx
@@ -2,10 +2,14 @@ export default function ArticleCard({ date, imageSrc = '', title, children }) {
   return (
     <div className="card bg-base-100 [ transition border duration-300 hover:shadow-xl ]">
       <figure>
-        <img className="w-full" src={imageSrc} alt="Shoes" />
+        <img
+          className="w-full md:h-56 object-cover"
+          src={imageSrc}
+          alt="Shoes"
+        />
       </figure>
       <div className="card-body">
-        <time className="font-light" dateTime={date}>
+        <time className="font-light text-primary" dateTime={date}>
           {date}
         </time>
         <h2 className="card-title">{title}</h2>
