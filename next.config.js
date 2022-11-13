41c2d777f9f694ca27b440205a1a90ec96b9fdf9
pabla <pablodelfantexp@gmail.com>
2022-11-13 15:36:27 -0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-
-function resolveBasePath() {
-    if (process.env.NODE_ENV === "development") return ''
-    if (process.env.NETLIFY_ENV) return ''
-    return "/web-project"
+function basePath() {
+  if (process.env.NODE_ENV === 'development') return ''
+  if (process.env.NETLIFY_ENV) return ''
+  if (process.env.GITHUB_ACTIONS) return '/web-project'
+  return ''
 }
 
-const BASE_PATH = resolveBasePath();
+const BASE_PATH = basePath()
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    ...BASE_PATH,
-    basePath: BASE_PATH
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  ...BASE_PATH,
+  basePath: BASE_PATH,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
