be1701191306a77231d4de2711d9f2dc033d71b3
pabla <pablodelfante@vexels..com>
2022-11-14 20:53:50 -0300

    reduce logic on build base path

--- utils/index.js
+++ utils/index.js
@@ -4,8 +4,6 @@ export function resolveBasePath(path) {
 }
 
 export function basePath() {
-  if (process.env.NODE_ENV === 'development') return ''
-  if (process.env.NETLIFY_ENV) return ''
   if (process.env.GITHUB_ACTIONS) return '/web-project'
   return ''
 }
