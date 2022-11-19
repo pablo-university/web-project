cd9377f266187c4225d09b6dbebd16ec3d0da23f
pabla <pablodelfante@vexels..com>
2022-11-19 00:36:34 -0300

    remove deprecated function

--- connectors/getArticles.js
+++ connectors/getArticles.js
@@ -7,36 +7,6 @@ export async function getArticles() {
   return articles
 }
 
-export async function getArticlesBeta() {
-  const myQuery = `{
-   AllArticles{
-      id,
-      date: createdAt,
-      updatedAt,
-      title,
-      subtitle,
-      description,
-      cover{url},
-      coverExtension{url},
-      published
-    }
-  }`
-  const apiRocketKey = process.env.API_ROCKET_KEY
-
-  const response = await fetch('https://graphql.apirocket.io', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${apiRocketKey}`,
-    },
-    body: JSON.stringify({ query: myQuery }),
-  })
-  const {
-    data: { AllArticles },
-  } = await response.json()
-  return AllArticles
-}
-
 export async function getArticlesHasura() {
   const query = `
     query MyQuery {
