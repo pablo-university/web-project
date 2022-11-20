15ad9c431e4804fc3f657b472912ad8090ac0218
pabla <pablodelfantexp@gmail.com>
2022-11-20 17:50:09 -0300

    implement articles backup

---
+++ pages/api/articles/backup.js
@@ -0,0 +1,6 @@
+import { getArticlesHasura } from 'connectors/getArticles'
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+  res.status(200).json(await getArticlesHasura())
+}
