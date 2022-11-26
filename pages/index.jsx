fbc057630528fc397dbbb545da9af1e58b7240f8
pabla <pablodelfante@vexels..com>
2022-11-26 15:12:01 -0300

    pr epr

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Layout from 'components/layouts/Layout'
 import Container from 'components/layouts/Container'
 import ContainerSeparator from 'components/layouts/ContainerSeparator'
