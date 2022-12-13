b156fda6507e03fe0b34739da9fbf0a20371d66a
Pablo Delfante <66787488+pablodelfante@users.noreply.github.com>
2022-12-13 15:57:41 -0300

    Develop integration (#13)
    * center content page and conditions
    
    * add image architecture new
    
    * Improvements cover (#9)
    
    * custom animate pulse
    
    * first base implementation animation transition backgrounds
    
    * refactor hero animation
    
    * rtemove log
    
    * pr epr
    
    * pre pr
    
    Co-authored-by: pabla <pablodelfante@vexels..com>
    
    * update architecture picture
    
    * docs: :zap: update architecture image
    
    * Form validation (#12)
    
    * docs: :zap: update architecture image
    
    * feat: :sparkles: implement basic validation on first form with react form library
    
    * feat: :sparkles: add react form library
    
    * feat: :sparkles: base implementation of validation form on personal dates
    
    * chore: :construction: remove unused code
    
    * feat: update patterns
    
    * refactor: :rocket: remove back button, will be a detail to solve
    
    * chore: :recycle: remove unneccesary code
    
    Co-authored-by: pabla <pablodelfante@vexels..com>
    
    Co-authored-by: pabla <pablodelfante@vexels..com>

--- pages/donate/personalDates.jsx
+++ pages/donate/personalDates.jsx
@@ -1,14 +1,20 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import ContainerGrid from 'components/layouts/ContainerGrid'
-import PrimaryOutlineButton from 'components/buttons/PrimaryOutlineButton'
 import PrimaryButton from 'components/buttons/PrimaryButton'
 import Input from 'components/form/Input'
 import Wrapper from 'components/pages/donate/Wrapper'
 import Select from 'components/form/Select'
 import { useContext, AppContext } from 'context/app'
+import { useForm } from 'react-hook-form'
 
 export default function ReservationDates() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
   const router = useRouter()
   const appContext = useContext(AppContext)
 
@@ -22,7 +28,7 @@ export default function ReservationDates() {
     router.push('/donate/')
   }
 
-  const handleClickNext = () => {
+  const handleClickNext = (formData) => {
     /**
      * si es valido asigno nuevo contexto de step
      * redirigir
@@ -35,35 +41,59 @@ export default function ReservationDates() {
         <h3>Datos Personales</h3>
         <ContainerGrid className="lg:grid-cols-2 lg:gap-6">
           <div>
-            <Input label="Nombre" placeholder="Juanito" />
-            {/**  
-          TODO
-          si hay tiempo resolver la parte de los input type radio
-          */}
-            {/* <div>
-            <ContainerGrid className="grid-cols-2">
-              <input
-                type="radio"
-                name="sexo"
-                className="radio radio-primary"
-                checked
-              />
-              <label className="label">
-                <span className="label-text">
-                  Pick the best fantasy franchise
-                </span>
-              </label>
-            </ContainerGrid>
-           
-          </div> 
-          <input type="radio" name="sexo" className="radio radio-primary" />
-           */}
-            <Input label="Cédula" type="number" placeholder="456464984" />
-            <Input label="Teléfono" type="number" placeholder="0988259998++" />
+            <Input
+              label="Nombre"
+              placeholder="Juanito"
+              register={register}
+              registerOptions={{
+                required: true,
+              }}
+              feedback={errors.Nombre ? 'Debes colocar un nombre' : ''}
+            />
+            <Input
+              label="Cédula"
+              type="number"
+              placeholder="456464984"
+              register={register}
+              registerOptions={{
+                required: true,
+              }}
+              feedback={errors.Cédula ? 'Debes colocar tu CI' : ''}
+            />
+            <Input
+              label="Teléfono"
+              type="number"
+              placeholder="0988259998++"
+              register={register}
+              registerOptions={{
+                required: true,
+                pattern: /\d{7,10}/,
+              }}
+              feedback={errors.Teléfono ? 'Necesitamos un teléfono válido' : ''}
+            />
           </div>
           <div>
-            <Input label="Correo" type="mail" placeholder="pablo@correo.com" />
-            <Input label="Dirección" type="text" placeholder="Rabmbla 2716" />
+            <Input
+              label="Correo"
+              type="mail"
+              placeholder="pablo@correo.com"
+              register={register}
+              registerOptions={{
+                required: true,
+                pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+              }}
+              feedback={errors.Correo ? 'Este email parece inválido' : ''}
+            />
+            <Input
+              label="Dirección"
+              type="text"
+              placeholder="Rabmbla 2716"
+              register={register}
+              registerOptions={{
+                required: true,
+              }}
+              feedback={errors.Dirección ? 'Necesitamos una Dirección' : ''}
+            />
             <Select
               label="Ciudad"
               options={[
@@ -75,23 +105,23 @@ export default function ReservationDates() {
                 'Flores',
                 'Florida',
               ]}
-              value="Artigas"
-              onChange={() => {}}
             >
               Selecionar una ciudad
             </Select>
           </div>
         </ContainerGrid>
       </div>
-      <div className="grid grid-cols-2">
-        <Link href="/donate/">
+      <div>
+        {/*  <Link href="/donate/">
           <a>
             <PrimaryOutlineButton onClick={handleClickBack}>
               Atrás
             </PrimaryOutlineButton>
           </a>
-        </Link>
-        <PrimaryButton onClick={handleClickNext}>Siguiente</PrimaryButton>
+        </Link> */}
+        <PrimaryButton onClick={handleSubmit(handleClickNext)}>
+          Siguiente
+        </PrimaryButton>
       </div>
     </Wrapper>
   )
