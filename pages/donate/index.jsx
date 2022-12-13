bb50e2a377ab9afdf01a8d31e94c40afe602935d
Pablo Delfante <66787488+pablodelfante@users.noreply.github.com>
2022-12-13 15:51:03 -0300

    Form validation (#12)
    * docs: :zap: update architecture image
    
    * feat: :sparkles: implement basic validation on first form with react form library
    
    * feat: :sparkles: add react form library
    
    * feat: :sparkles: base implementation of validation form on personal dates
    
    * chore: :construction: remove unused code
    
    * feat: update patterns
    
    * refactor: :rocket: remove back button, will be a detail to solve
    
    * chore: :recycle: remove unneccesary code
    
    Co-authored-by: pabla <pablodelfante@vexels..com>

--- pages/donate/index.jsx
+++ pages/donate/index.jsx
@@ -1,19 +1,25 @@
-import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import ContainerGrid from 'components/layouts/ContainerGrid'
 import PrimaryButton from 'components/buttons/PrimaryButton'
 import Input from 'components/form/Input'
 import Wrapper from 'components/pages/donate/Wrapper'
 import { useContext, AppContext } from 'context/app'
+import { useForm } from 'react-hook-form'
 
 export default function ReservationDates() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
   const router = useRouter()
   const appContext = useContext(AppContext)
 
-  const handleClickNextStep = () => {
+  const handleClickNextStep = (data) => {
     /**
-     * si es valido asigno nuevo contexto de step
-     * redirigir
+     * TODO:
+     * se deberían poder cargar datos viejos si el usuario desea volver a esta parte del formulario
      */
     appContext.setContextState({
       ...appContext,
@@ -23,24 +29,55 @@ export default function ReservationDates() {
     })
     router.push('/donate/personalDates')
   }
+
   return (
     <Wrapper>
       <div>
         <h3>Datos de la reserva</h3>
         <ContainerGrid className="lg:grid-cols-2 lg:gap-6">
           <div>
-            <Input label="Localidad" placeholder="Ingresa una localidad" />
-            <Input label="Motivo" placeholder="Ingresa motivo de donación" />
+            <Input
+              label="Localidad"
+              placeholder="Ingresa una localidad"
+              register={register}
+              registerOptions={{
+                required: true,
+              }}
+              feedback={
+                errors.Localidad
+                  ? 'Debes colocar una localidad de tu preferencia'
+                  : ''
+              }
+            />
+            <Input
+              register={register}
+              label="Motivo"
+              placeholder="Ingresa motivo de donación"
+              registerOptions={{
+                required: true,
+              }}
+              feedback={
+                errors.Localidad ? 'Debes brindar un motivo de donación' : ''
+              }
+            />
           </div>
           <div>
-            <Input label="Fecha" type="datetime-local" />
+            <Input
+              register={register}
+              registerOptions={{
+                required: true,
+              }}
+              feedback={
+                errors.Localidad ? 'Debes colocar una fecha para donar' : ''
+              }
+              label="Fecha"
+              type="datetime-local"
+            />
           </div>
         </ContainerGrid>
-        {/*  <Link href="/donate/personalDates">
-          <a> */}
-        <PrimaryButton onClick={handleClickNextStep}>Siguiente</PrimaryButton>
-        {/*  </a>
-        </Link> */}
+        <PrimaryButton onClick={handleSubmit(handleClickNextStep)}>
+          Siguiente
+        </PrimaryButton>
       </div>
     </Wrapper>
   )
